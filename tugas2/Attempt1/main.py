235d391960dbbaaa8f80f87efa2e7662415c842b
nandakho <nandakho@nandakho.my.id>
2020-11-16 11:29:09 +0700

    add tugas2

---
+++ tugas2/Attempt1/main.py
@@ -0,0 +1,31 @@
+from pandas import read_csv
+
+kriteria = ["sepal.length", "sepal.width", "petal.length", "petal.width"]
+dataset = read_csv('iris.csv')  # csv name
+category = dataset['variety'].unique()
+test = dataset.groupby('variety')
+inp = [0] * len(kriteria)
+jenis = [0] * len(category)
+
+print("Insert your data for prediction")
+for k in range(len(kriteria)):
+    inp_prompt = "Insert " + kriteria[k] + ": "
+    inp[k] = float(input(inp_prompt))
+print("")
+
+for c in range(len(category)):
+    # print(category[c])
+    for k in range(len(kriteria)):
+        # print(kriteria[k], ":", test[kriteria[k]].min()[c], "<=", inp[k], "<=", test[kriteria[k]].max()[c])
+        if test[kriteria[k]].min()[c] <= inp[k] <= test[kriteria[k]].max()[c]:
+            jenis[c] += 1
+            # print("true")
+    # print("")
+
+# print(jenis)
+
+print("Result:")
+for j in range(len(jenis)):
+    print(jenis[j] / len(kriteria) * 100, "%", category[j])
+
+print("Your input is a", category[jenis.index(max(jenis))])
---
+++ tugas2/Attempt1/readme.txt
@@ -0,0 +1,14 @@
+Library yang digunakan:
+Membutuhkan library pandas.
+
+Algorithm:
+Code akan menimport iris.csv sebagai data acuan, dan mengambil nilai min dan max dari setiap kategori iris.
+User diminta memasukkan nilai kriteria untuk melakukan prediksi.
+Jika nilai yang dimasukkan user berada diantara min dan max dari suatu kategori, nilai sesuai + 1, dan langkah ini dilakukan untuk setiap kategori.
+Nilai presentasi prediksi ditampilkan dengan membandingkan nilai sesuai setiap kategori dengan jumlah kriteria.
+Nilai max dari nilai sesuai akan ditampilkan sebagai hasil prediksi.
+
+Komentar:
+Algoritma ini mungkin berfungsi untuk dataset yang memiliki nilai integer atau float, namun untuk dataset ber tipe data string sepertinya tidak bisa.
+
+-- Oleh Ananda Khosuri --
